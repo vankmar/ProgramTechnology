8be8230e05f195b977137654a13d3cdcd3d8adb5
vankmar <l05kku70kk@gmail.com>
2026-09-25 15:06:15 +0700

    #1 Созданы классы BankAccount и Transaction

---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Bank
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            BankAccount account1 = new BankAccount("Marina", 1000000000);
+            BankAccount account2 = new BankAccount("Liza", 1000);
+
+            Console.WriteLine($"{account1.Owner} {account1.Balance} {account1.Number}");
+            Console.WriteLine($"{account2.Owner} {account2.Balance} {account2.Number}");
+
+            account1.MakeDeposit(6767, DateTime.UtcNow, ":)");
+            Console.WriteLine($"Balance: {account1.Balance}");
+            account1.MakeWithdrawal(10000, DateTime.UtcNow, ":(");
+            Console.WriteLine($"Balance: {account1.Balance}");
+
+            try
+            {
+                account2.MakeWithdrawal(10000000, DateTime.UtcNow, ":(");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
+
---
+++ Bank/Bank/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace Bank;
+
+/// <summary>
+/// Тип данных, который запрещает менять состояние объекта
+/// </summary>
+/// <param name="Amount"> сумма транзакции </param>
+/// <param name="Date"> дата транзакции </param>
+/// <param name="Note"> заметки к транзакции </param>
+internal record Transaction(decimal Amount, DateTime Date, string Note); // тк транс - неизм-й объект (запись)
+
+//internal record Transaction
+//{
+//    public decimal Amount { get; }
+//    public DateTime Date { get; }
+//    // и т.д. - в (decimal Amount, DateTime Date, string Note)
+//}
+
+//trr dgsgdfgdf
